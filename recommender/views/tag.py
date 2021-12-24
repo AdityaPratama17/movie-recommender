b28ebf435273d1e85634882313e359f30b5de36b
arisurya <arisurya425@gmail.com>
2021-12-24 19:27:04 +0800

    Display data from neo4j

--- recommender/views/tag.py
+++ recommender/views/tag.py
@@ -1,10 +1,17 @@
-from django.http.response import HttpResponse
+import json
 from django.shortcuts import render
 from neo4j import GraphDatabase
 
 def tag(request):
+    graphdb = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j","1234"))
+    session = graphdb.session()
+    query = "MATCH(m:Movie)-[r:isTaggedBy]->(u:User) RETURN u.ID AS id_user, m.ID AS id_movie, m.Title AS title, collect(r.Tag) AS tag ORDER BY id_user"
+    nodes = session.run(query)
+    data = json.dumps(nodes.data())
+    data = json.loads(data)
     context ={
         'isTag' : True,
-        'title':'Tag'
+        'title':'Tag',
+        'data':data
     }
     return render(request, 'recommender/tag/index.html', context)
